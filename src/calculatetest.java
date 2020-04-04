d91c7f00e4a6511f9c595db84bec0ecaa027c568
titusat <41177682+titusat@users.noreply.github.com>
2020-04-04 19:32:30 -0400

    Create calculatetest.java

---
+++ src/calculatetest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+ 
+/**
+ * This is test case class for division method.
+ * @author codesjava
+ */
+public class calculatetest {
+	//DivisionTest class objects
+	DivisionTest divisionTest1 = new DivisionTest(10, 2); 
+	DivisionTest divisionTest2 = new DivisionTest(10, 0); 
+ 
+	//Test case for division
+	@Test
+	public void test() {
+		assertEquals(5, divisionTest1.division());
+	}
+ 
+	//Test case for expected ArithmeticException, 
+	//As in this case ArithmeticException
+	// is the expected exception so JUnit 
+	//will pass this unit test. 
+	@Test(expected = ArithmeticException.class)  
+	public void testException() {
+		assertEquals(5, divisionTest2.division());
+	}
+}
