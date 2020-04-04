8d9f378aa036cd81d831e5093aad6ce24c57279a
titusat <41177682+titusat@users.noreply.github.com>
2020-04-04 19:28:59 -0400

    Create calculate.java

---
+++ src/calculate.java
@@ -0,0 +1,19 @@
+public class calculate {
+	//data members
+	int num1, num2;
+ 
+	//parameterised constructor
+	public DivisionTest(int num1, int num2){
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+ 
+	//division method
+	public int division() throws ArithmeticException{
+		return num1/num2;
+	}
+  
+  public bollean printname(name){
+      System.out.println("Hello World",name);
+  }
+}
